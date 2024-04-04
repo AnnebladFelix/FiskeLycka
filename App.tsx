a1ae116997ee0c280706ad6ad4fc4a05bc99b614
Therese Hagwall <theresejager@live.se>
2024-04-04 11:02:29 +0200

    changing background color

--- App.tsx
+++ App.tsx
@@ -1,21 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hej Bajskorv</Text>
-      <Text>Hej igen Bajskorv</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hej Bajskorv</Text>
+            <Text style={styles.text}>Hej igen Bajskorv</Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "black",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+      color: 'white'
+    }
 });
