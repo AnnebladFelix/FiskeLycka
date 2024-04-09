3e6fa36b9524df97a8b5f0452f18568925b51d12
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-04-09 15:31:50 +0200

    testing

--- App.tsx
+++ App.tsx
@@ -1,21 +1,63 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {ImageBackground, StyleSheet, Text, TouchableOpacity, View, Image,} from 'react-native';
 
 export default function App() {
+  const handleMenuClick = () => {
+    console.log('Menu clicked');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hej Bajskorv</Text>
-      <Text>Hej igen Bajskorv</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./assets/images/bakground1.jpg')}
+      style={styles.background}
+    >
+      {/* Header */ }
+      <View style={styles.header}>
+        <Text>Header</Text>
+        <TouchableOpacity style={styles.menuButton} onPress={handleMenuClick}>
+        <Image source={require('./assets/images/icon-hamburger.png')} style={styles.icon} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Body */ }
+      <View style={styles.container}>
+        <Text>Hej Bajskorv</Text>
+        <Text>Hej igen Bajskorv</Text>
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    height: 100,
+    paddingHorizontal: 20,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+  },
+  menuButton: {
+    padding: 10,
+  },
+  icon: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
   },
 });
