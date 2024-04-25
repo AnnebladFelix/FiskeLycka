8c7a033459cbd9d9ce352d12baba386d0c295c01
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-04-25 16:01:27 +0200

    layout and making map button connect to MapScreen

---
+++ src/screens/MapScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MapScreen = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Map Screen</Text>
+      {/* Your map component would go here */}
+    </View>
+  );
+}
+
+export default MapScreen;
