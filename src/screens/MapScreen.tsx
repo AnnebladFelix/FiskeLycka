e536e1f523ce626b81591f5f03ec891070fd1ee7
Matias Bengtsson <bengtsson1985@hotmail.com>
2024-04-30 14:58:06 +0200

    fixed the mapView

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
 
 const MapScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Map Screen</Text>
-      {/* Your map component would go here */}
+    <View style={styles.container}>
+      <Text>Hitta din nya fiskeplats</Text>
+
+      <MapView style={styles.map}>
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    
+  },
+  map: {
+    width: '100%',
+    height: '50%',
+  }
+})
 
 export default MapScreen;
