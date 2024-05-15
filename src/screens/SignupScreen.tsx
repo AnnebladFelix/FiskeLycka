990732fe199f2204b33a1b09587c7a91a8fd027c
Felix Anneblad <felix.anneblad@gmail.com>
2024-05-15 18:38:50 +0200

    Update the Prisma schema of the `authorId` and `id` from `Int` to `String`. Add uuid npm package to the Prisma schema. clean up the SignupScreen.

--- src/screens/SignupScreen.tsx
+++ src/screens/SignupScreen.tsx
@@ -1,34 +1,31 @@
-import React, { useState } from 'react';
-import { Button, TextInput, View } from 'react-native';
-import { createUser } from '../../db/userOperations'; // adjust the path according to your project structure
+import React, { useState } from "react";
+import { Button, TextInput, View } from "react-native";
+import { createUser } from "../../db/userOperations";
 
 const SignupScreen = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignup = async () => {
     try {
-      const newUser = await createUser(email, name, password);
-      console.log(`Created new user: ${newUser.email} (ID: ${newUser.id})`);
-      // Navigate to another screen, show a success message, etc.
-    } catch (error: Error) {
+      await createUser(email, name, password);
+    } catch (error: any) {
       console.error(`Something went wrong: ${error.message}`);
-      // Show an error message, etc.
     }
   };
 
   return (
     <View>
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Email"
+      <TextInput 
+        value={email} 
+        onChangeText={setEmail} 
+        placeholder="Email" 
       />
-      <TextInput
-        value={name}
-        onChangeText={setName}
-        placeholder="Name"
+      <TextInput 
+        value={name} 
+        onChangeText={setName} 
+        placeholder="Name" 
       />
       <TextInput
         value={password}
@@ -36,7 +33,10 @@ const SignupScreen = () => {
         placeholder="Password"
         secureTextEntry
       />
-      <Button title="Sign Up" onPress={handleSignup} />
+      <Button 
+        title="Sign Up" 
+        onPress={handleSignup} 
+      />
     </View>
   );
 };
