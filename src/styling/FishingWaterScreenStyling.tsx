d8ed7cf4f1f3cb3eb0ecadc0342904e7b9cdd4b9
Therese Hagwall <theresejager@live.se>
2024-06-03 21:00:04 +0200

    Move fishing waterscreen styling

--- src/styling/FishingWaterScreenStyling.tsx
+++ src/styling/FishingWaterScreenStyling.tsx
@@ -1,47 +1,71 @@
 import { StyleSheet } from 'react-native';
 
 export const FishingWaterScreenStyle = StyleSheet.create({
-    wrapper: {
-      flex: 1,
-    },
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      padding: 20,
-    },
-    scrollView: {
-      width: '100%',
-    },
-    title: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginBottom: 20,
-      textAlign: 'center',
-    },
-    text: {
-      fontSize: 16,
-      textAlign: 'center',
-    },
-    image: {
-      width: 300,
-      height: 200,
-      resizeMode: 'contain',
-    },
-    button: {
-      backgroundColor: '#4CAF50',
-      padding: 10,
-      marginVertical: 8,
-      borderRadius: 5,
-      alignItems: 'center',
-    },
-    buttonText: {
-      color: 'white',
-      fontSize: 16,
-    },
-    noFishText: {
-      fontSize: 16,
-      color: 'red',
-      textAlign: 'center',
-    },
+  wrapper: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  scrollView: {
+    width: '100%',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'center',
+  },
+  image: {
+    width: 300,
+    height: 200,
+    resizeMode: 'contain',
+  },
+  card: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 3,
+  },
+  cardTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  cardSection: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  linkText: {
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    padding: 10,
+    marginVertical: 8,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  noFishText: {
+    fontSize: 16,
+    color: 'red',
+    textAlign: 'center',
+  },
   });
