e386a7bd08b7f2a9d7f5c7c4c5bbe3adbb0b84be
Felix Anneblad <felix.anneblad@gmail.com>
2024-06-04 10:28:51 +0200

    chore: styling on fishdetails

--- src/screens/fishScreens/FishDetailScreen.tsx
+++ src/screens/fishScreens/FishDetailScreen.tsx
@@ -1,99 +1,105 @@
 import React, { useEffect, useState } from "react";
-import { View, Text, Image, ActivityIndicator } from "react-native";
+import {
+  View,
+  Text,
+  Image,
+  ActivityIndicator,
+  ImageBackground,
+} from "react-native";
 import axios from "axios";
 import Header from "../../components/Header";
 import { FishDetailScreenStyling as styles } from "../../styling/FishDetailScreenStyling";
 
 interface FishSpecies {
-    swedishName: string;
-    scientificName: string;
-    description: string;
-    occurrence: string;
-    habitat: string;
-    minimumSize: string;
-    swedishRecord: string;
-    lake: string;
+  swedishName: string;
+  scientificName: string;
+  description: string;
+  occurrence: string;
+  habitat: string;
+  minimumSize: string;
+  swedishRecord: string;
+  lake: string;
 }
 
 const FishDetailScreen = ({ route }: { route: any }) => {
-    const fish: FishSpecies = route.params.fish;
+  const fish: FishSpecies = route.params.fish;
 
-    const [fishImage, setFishImage] = useState<string | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
+  const [fishImage, setFishImage] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
 
-    useEffect(() => {
-        const fetchFishImage = async () => {
-            try {
-                const response = await axios.get(
-                    "https://sv.wikipedia.org/w/api.php",
-                    {
-                        params: {
-                            action: "query",
-                            prop: "pageimages|pageterms",
-                            pithumbsize: 500,
-                            titles: fish.swedishName,
-                            format: "json",
-                            origin: "*",
-                        },
-                    }
-                );
+  useEffect(() => {
+    const fetchFishImage = async () => {
+      try {
+        const response = await axios.get("https://sv.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            prop: "pageimages|pageterms",
+            pithumbsize: 500,
+            titles: fish.swedishName,
+            format: "json",
+            origin: "*",
+          },
+        });
 
-                const pages = response.data.query.pages;
-                const pageId = Object.keys(pages)[0];
+        const pages = response.data.query.pages;
+        const pageId = Object.keys(pages)[0];
 
-                if (pageId !== "-1" && pages[pageId].thumbnail) {
-                    setFishImage(pages[pageId].thumbnail.source);
-                } else {
-                    setError("Ingen bild hittades för denna fiskart");
-                }
-            } catch (error) {
-                console.error(error);
-                setError("An error occurred while fetching the fish image");
-            } finally {
-                setLoading(false);
-            }
-        };
+        if (pageId !== "-1" && pages[pageId].thumbnail) {
+          setFishImage(pages[pageId].thumbnail.source);
+        } else {
+          setError("Ingen bild hittades för denna fiskart");
+        }
+      } catch (error) {
+        console.error(error);
+        setError("An error occurred while fetching the fish image");
+      } finally {
+        setLoading(false);
+      }
+    };
 
-        fetchFishImage();
-    }, [fish.swedishName]);
+    fetchFishImage();
+  }, [fish.swedishName]);
 
-    return (
-        <View style={styles.container}>
-            <Header />
-            <View style={styles.content}>
-                <Text style={styles.mainHeaderText}>{fish.swedishName}</Text>
-                <Text style={styles.subHeaderText}>
-                    ({fish.scientificName})
-                </Text>
-                {loading ? (
-                    <ActivityIndicator size="large" color="#0000ff" />
-                ) : error ? (
-                    <Text style={styles.errorText}>{error}</Text>
-                ) : fishImage ? (
-                    <Image
-                        source={{ uri: fishImage }}
-                        style={styles.image}
-                        resizeMode="contain"
-                    />
-                ) : (
-                    <Text style={styles.bodyText}>
-                        Ingen bild hittades för denna fiskart
-                    </Text>
-                )}
-                <Text style={styles.headerText}>Beskrivning</Text>
-                <Text style={styles.bodyText}>{fish.description}</Text>
-                <Text style={styles.headerText}>Förekomst</Text>
-                <Text style={styles.bodyText}>{fish.occurrence}</Text>
-                <Text style={styles.headerText}>Habitat</Text>
-                <Text style={styles.bodyText}>{fish.habitat}</Text>
-                <Text style={styles.headerText}>Minsta storlek</Text>
-                <Text style={styles.bodyText}>{fish.minimumSize}</Text>
-                <Text style={styles.headerText}>Svenskt rekord</Text>
-                <Text style={styles.bodyText}>{fish.swedishRecord}</Text>
-            </View>
+  return (
+    <ImageBackground
+      source={require("../../../assets/images/bakground1.jpg")}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Header />
+        <View style={styles.card}>
+          <Text style={styles.mainHeaderText}>{fish.swedishName}</Text>
+          <Text style={styles.subHeaderText}>({fish.scientificName})</Text>
+          {loading ? (
+            <ActivityIndicator size="large" color="#0000ff" />
+          ) : error ? (
+            <Text style={styles.errorText}>{error}</Text>
+          ) : fishImage ? (
+            <Image
+              source={{ uri: fishImage }}
+              style={styles.image}
+              resizeMode="contain"
+            />
+          ) : (
+            <Text style={styles.bodyText}>
+              Ingen bild hittades för denna fiskart
+            </Text>
+          )}
+          <Text style={styles.headerText}>Beskrivning</Text>
+          <Text style={styles.bodyText}>{fish.description}</Text>
+          <Text style={styles.headerText}>Förekomst</Text>
+          <Text style={styles.bodyText}>{fish.occurrence}</Text>
+          <Text style={styles.headerText}>Habitat</Text>
+          <Text style={styles.bodyText}>{fish.habitat}</Text>
+          <Text style={styles.headerText}>Minsta storlek</Text>
+          <Text style={styles.bodyText}>{fish.minimumSize}</Text>
+          <Text style={styles.headerText}>Svenskt rekord</Text>
+          <Text style={styles.bodyText}>{fish.swedishRecord}</Text>
         </View>
-    );
+      </View>
+    </ImageBackground>
+  );
 };
 
 export default FishDetailScreen;
--- src/styling/FishDetailScreenStyling.tsx
+++ src/styling/FishDetailScreenStyling.tsx
@@ -1,42 +1,59 @@
 import { StyleSheet } from "react-native";
 
 export const FishDetailScreenStyling = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#F5FCF7",
-      },
-      mainHeaderText:{
-        fontSize:24,
-        fontWeight: 'bold',
-        marginTop:10,
-      },
-      content: {
-        padding: 10,
-    },
-    subHeaderText: {
-      fontSize: 16,
-    },
-    headerText: {
-      fontWeight: 'bold',
-      fontSize: 18,
-      marginTop: 10,
-    },
-    bodyText: {
-      fontSize: 16,
-      marginBottom: 10,
-    },
-    image: {
-        width: 400,
-        height: 300,
-        alignSelf: "center",
-        margin: 5, 
-    },
-    errorText: {
-        fontSize:17,
-        color: "red",
-        textAlign: "center",
-    },
-    loadingIndicator: {
-        marginTop: 20,
-    },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'flex-start',
+  },
+  container: {
+    flex: 1,
+  },
+  mainHeaderText: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  content: {
+    padding: 10,
+  },
+  subHeaderText: {
+    fontSize: 16,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 18,
+    marginTop: 10,
+  },
+  bodyText: {
+    fontSize: 16,
+    marginBottom: 10,
+  },
+  errorText: {
+    fontSize: 17,
+    color: "red",
+    textAlign: "center",
+  },
+  loadingIndicator: {
+    marginTop: 20,
+  },
+  card: {
+    backgroundColor: "#f9f9f9",
+    padding: 25,
+    margin: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+  image: {
+    width: "100%",
+    height: 300,
+    borderRadius: 10,
+    alignSelf: "center",
+    margin: 5,
+  },
 });
