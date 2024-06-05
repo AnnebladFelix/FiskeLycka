ef56a6dffc0983fcb309b7e98cf8c7397cd888a4
Tony Hagwall <hagwall86@live.se>
2024-06-05 11:03:52 +0200

    added loading

--- src/screens/userScreens/GetAllUsersScreen.tsx
+++ src/screens/userScreens/GetAllUsersScreen.tsx
@@ -1,16 +1,18 @@
 import React, { useEffect, useState } from "react";
 import { UserData } from "../../interfaces/userInterfaces";
-import { View, Text, ImageBackground } from "react-native";
+import { View, Text, ImageBackground, ActivityIndicator } from "react-native";
 import { fetchUsers } from "../../../db/userOperations";
 import Header from "../../components/Header";
 import { GetAllUsersScreenStyling as styles } from "../../styling/GetAllUsersScreenStyling";
 
 function GetAllUsersScreen() {
   const [users, setUsers] = useState<UserData[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const getUsers = async () => {
       const allUsers = await fetchUsers();
+      setLoading(false);
 
       const userData: UserData[] = allUsers.map((user) => ({
         ...user,
@@ -25,31 +27,41 @@ function GetAllUsersScreen() {
   }, []);
 
   return (
-      <ImageBackground
-            source={require("../../../assets/images/bakground1.jpg")}
-            style={styles.background}
-        >
+    <ImageBackground
+      source={require("../../../assets/images/bakground1.jpg")}
+      style={styles.background}
+    >
       <Header />
-      {users.map((user, index) => (
-        <View key={index}>
-          <View style={styles.card}>
-            <Text>Användarnamn: {user.name}</Text>
-            <Text>E-mail: {user.email}</Text>
-            {user.posts && user.posts.length > 0 ? (
-              user.posts.map((post, postIndex) => (
-                <Text key={postIndex}>
-                  Post: Rubrik: {String(post.title)} Innehåll:{" "}
-                  {String(post.content)}{" "}
-                </Text>
-              ))
-            ) : (
-              <View>
-                <Text>Den här användaren har inte lagt ut några POSTS än</Text>
-              </View>
-            )}
+      {loading ? (
+        <ActivityIndicator
+          style={styles.loadingIndicator}
+          size={60}
+          color="#0000ff"
+        />
+      ) : (
+        users.map((user, index) => (
+          <View key={index}>
+            <View style={styles.card}>
+              <Text>Användarnamn: {user.name}</Text>
+              <Text>E-mail: {user.email}</Text>
+              {user.posts && user.posts.length > 0 ? (
+                user.posts.map((post, postIndex) => (
+                  <Text key={postIndex}>
+                    Post: Rubrik: {String(post.title)} Innehåll:{" "}
+                    {String(post.content)}{" "}
+                  </Text>
+                ))
+              ) : (
+                <View>
+                  <Text>
+                    Den här användaren har inte lagt ut några POSTS än
+                  </Text>
+                </View>
+              )}
+            </View>
           </View>
-        </View>
-      ))}
+        ))
+      )}
     </ImageBackground>
   );
 }
